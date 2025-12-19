a11c8bfd7c7de5e6de7572f2a26d4bc215bd364f
Akshay Kaushik <kaushikakshay@google.com>
2025-12-19 09:20:57 +0000

    chore: finalize react 19 upgrade and clean up files

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -53,10 +53,7 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            "presets": [
-              "@babel/preset-env",
-              ["@babel/preset-react", { "runtime": "automatic" }]
-            ],
+            "presets": ["@babel/preset-env",["@babel/preset-react", { "runtime": "automatic" }]],
             "plugins": ["@babel/plugin-transform-runtime", "@babel/plugin-proposal-class-properties"]
           }
         }
